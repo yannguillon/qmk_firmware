6616e26518cfc80e0ce11ddbf8f94588100f1bc4
Yann <yann@dockdev.com>
2020-12-10 12:22:54 +0100

    overlox mapping

---
+++ keyboards/ergodox_infinity/keymaps/overlox/rules.mk
@@ -0,0 +1,35 @@
+# project specific files
+SRC =	matrix.c \
+	led.c
+
+BOOTMAGIC_ENABLE = no  # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE  = yes # Mouse keys(+4700)
+EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
+CONSOLE_ENABLE   = no  # Console for debug(+400)
+COMMAND_ENABLE   = yes # Commands for debug and configuration
+CUSTOM_MATRIX    = yes # Custom matrix file for the ErgoDox EZ
+SLEEP_LED_ENABLE = yes # Breathing sleep LED during USB suspend
+NKRO_ENABLE      = yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+UNICODE_ENABLE   = yes # Unicode
+SWAP_HANDS_ENABLE = yes # Allow swapping hands of keyboard
+STENO_ENABLE = yes  #enable plover steno mode
+CUSTOM_MATRIX = yes # Custom matrix file
+SERIAL_LINK_ENABLE = yes
+VISUALIZER_ENABLE = yes
+LCD_ENABLE = yes
+BACKLIGHT_ENABLE = yes
+LCD_BACKLIGHT_ENABLE = yes
+MIDI_ENABLE = yes
+MIDI_ADVANCED = yes
+MUSIC_MODE = yes
+RGBLIGHT_ENABLE = no
+
+LCD_DRIVER = st7565
+LCD_WIDTH = 128
+LCD_HEIGHT = 32
+
+LED_DRIVER = is31fl3731c
+LED_WIDTH = 7
+LED_HEIGHT = 7
+
+LAYOUTS = ergodox
